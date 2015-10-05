0066d0c099c403ec2b8191ee5407f7bd2f5944fd
Your Name <you@example.com>
2015-10-05 13:49:46 +0200

    first

---
+++ src/idemain.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+
+#include <QWidget>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication application(argc, argv);
+    QWidget widget;
+    widget.show();
+
+    return application.exec();
+}
