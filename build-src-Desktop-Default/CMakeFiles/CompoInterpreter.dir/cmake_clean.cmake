b57b2a3110c420c28a204a29f127b9c82a6324ea
Crikka <crikka@hotmail.fr>
2015-10-24 19:32:31 +0200

    removing build...

--- build-src-Desktop-Default/CMakeFiles/CompoInterpreter.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libCompoInterpreter.pdb"
-  "libCompoInterpreter.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/CompoInterpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build-src-Desktop-Default/CMakeFiles/CompoMetamodel.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CompoMetamodel.
-# This may be replaced when dependencies are built.
